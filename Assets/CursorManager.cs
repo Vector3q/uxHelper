80c2b5f2a08b5737b6845c39676e9a42ca5baff8
Vector3q <965024754@qq.com>
2023-11-27 18:25:46 +0800

    Keep Aspect Ratio of Videos

---
+++ Assets/CursorManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum UIEdge
+{
+    None,
+    Up,
+    Down,
+    Left,
+    Right,
+    TopRightCorner,
+    BottomRightCorner,
+    TopLeftCorner,
+    BottomLeftCorner
+}
+
+public class CursorManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/KeepAspectRatio.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepAspectRatio : MonoBehaviour
+{
+    public float aspectRatio = 16.0f / 9f;
+
+    private RectTransform rectTransform;
+    private Vector2 originalSize;
+
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        originalSize = rectTransform.sizeDelta;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RectTransform parentRectTransform = transform.parent.GetComponent<RectTransform>();
+
+        if (parentRectTransform != null)
+        {
+            // 获取父物体的宽度和高度
+            float parentWidth = parentRectTransform.rect.width;
+            float parentHeight = parentRectTransform.rect.height;
+
+            // 计算新的宽度和高度，保持长宽比
+            float newWidth = parentWidth;
+            float newHeight = parentWidth / aspectRatio;
+
+            if (newHeight > parentHeight)
+            {
+                newHeight = parentHeight;
+                newWidth = parentHeight * aspectRatio;
+            }
+
+            // 设置子物体的大小
+            rectTransform.sizeDelta = new Vector2(newWidth, newHeight);
+        }
+    }
+}
+
