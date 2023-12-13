536a62e3ea86014ba2b1eec94408f77c4f1b8cad
Vector3q <965024754@qq.com>
2023-12-13 18:44:52 +0800

    temporary voice input

--- Assets/Scripts/UIs/AddCommentIcon.cs
+++ Assets/Scripts/UIs/AddCommentIcon.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using System.IO;
+using HuggingFace.API;
 public class AddCommentIcon : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
 {
     // Start is called before the first frame update
     public Text text;
+    public Text SpeechRecognitionText;
     public Image image;
     public Slider playSlider;
     public Sprite BeforeclickedSprite;
     public Sprite AfterclickedSprite;
+    public GameObject textPanel;
+
+
+
+    private AudioClip clip;
+    private byte[] bytes;
+
     private bool IsClick;
+    
     void Start()
     {
         IsClick = false;
@@ -21,6 +32,7 @@ public class AddCommentIcon : MonoBehaviour, IPointerEnterHandler, IPointerExitH
     void Update()
     {
         this.transform.position = new Vector3(playSlider.handleRect.position.x, playSlider.handleRect.position.y, playSlider.handleRect.position.z);
+
     }
 
     public void OnPointerEnter(PointerEventData eventData)
@@ -47,11 +59,73 @@ public class AddCommentIcon : MonoBehaviour, IPointerEnterHandler, IPointerExitH
         if (IsClick)
         {
             image.sprite = AfterclickedSprite;
-
+            textPanel.SetActive(true);
         }
         else
         {
             image.sprite = BeforeclickedSprite;
         }
+        Recording();
+    }
+
+    private void Recording()
+    {
+        if (IsClick)
+        {
+            clip = Microphone.Start(null, false, 10, 44100);
+        }
+        else
+        {
+            var position = Microphone.GetPosition(null);
+            Microphone.End(null);
+            var samples = new float[position * clip.channels];
+            clip.GetData(samples, 0);
+            bytes = EncodeAsWAV(samples, clip.frequency, clip.channels);
+            SendRecording();
+        }
+    }
+
+    // Encode audio clip as wav file
+    private byte[] EncodeAsWAV(float[] samples, int frequency, int channels)
+    {
+        using (var memoryStream = new MemoryStream(44 + samples.Length * 2))
+        {
+            using (var writer = new BinaryWriter(memoryStream))
+            {
+                writer.Write("RIFF".ToCharArray());
+                writer.Write(36 + samples.Length * 2);
+                writer.Write("WAVE".ToCharArray());
+                writer.Write("fmt ".ToCharArray());
+                writer.Write(16);
+                writer.Write((ushort)1);
+                writer.Write((ushort)channels);
+                writer.Write(frequency);
+                writer.Write(frequency * channels * 2);
+                writer.Write((ushort)(channels * 2));
+                writer.Write((ushort)16);
+                writer.Write("data".ToCharArray());
+                writer.Write(samples.Length * 2);
+
+                foreach (var sample in samples)
+                {
+                    writer.Write((short)(sample * short.MaxValue));
+                }
+            }
+            return memoryStream.ToArray();
+        }
+    }
+
+
+    // Voice Recognition by Huggingface API
+    private void SendRecording()
+    {
+        
+        HuggingFaceAPI.AutomaticSpeechRecognition(bytes, response => {
+            SpeechRecognitionText.color = Color.white;
+            SpeechRecognitionText.text = response;
+        }, error => {
+            SpeechRecognitionText.color = Color.red;
+            SpeechRecognitionText.text = error;
+        });
     }
 }
